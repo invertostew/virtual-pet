b4d676d787f7052dd8d7ee374362e234c1a98109
Stuart Green <github.47a8z@aleeas.com>
2021-12-12 22:32:57 +0000

    set up failing test for growUp

--- __tests__/virtual-pet.test.js
+++ __tests__/virtual-pet.test.js
@@ -1,14 +1,32 @@
 // require Pet constructor function
 const Pet = require('../src/virtual-pet.js');
 
+let fido, bronson;
+beforeEach(() => {
+    fido = new Pet('Fido');
+    bronson = new Pet('Bronson');
+})
+
 describe('Pet', () => {
     test('Returns an instance of Pet', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Pet);
     });
     test('Returns the pets name', () => {
-        const fido = new Pet('Fido');
-        const bronson = new Pet('Bronson');
-        expect(fido.name).toEqual('Fido');
-        expect(bronson.name).toEqual('Bronson');
+        expect(fido.name).toBe('Fido');
+        expect(bronson.name).toBe('Bronson');
+    });
+});
+
+describe('growUp', () => {
+    test('Returns the initial age (should be 0)', () => {
+        expect(fido.age).toBe(0);
+        expect(bronson.age).toBe(0);
+    });
+    test('growUp increases the age property by 1', () => {
+        expect(fido.age).toBe(0);
+        fido.growUp();
+        expect(fido.age).toBe(1);
+        fido.growUp();
+        expect(fido.age).toBe(2);
     });
 });
