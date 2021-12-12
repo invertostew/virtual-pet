cd23bc713513abfd0754d8fea72612a64254128a
Stuart Green <github.47a8z@aleeas.com>
2021-12-12 22:18:15 +0000

    failing test for pet name

--- __tests__/virtual-pet.test.js
+++ __tests__/virtual-pet.test.js
@@ -5,4 +5,8 @@ describe('Pet', () => {
     test('Returns an instance of Pet', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Pet);
     });
+    test('Returns the pets name', () => {
+        const fido = new Pet('Fido');
+        expect(fido.name).toEqual('Fido');
+    });
 });
