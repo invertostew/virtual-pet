eaf99b409900d4a1077019e13a076b7e157d9438
Stuart Green <github.47a8z@aleeas.com>
2021-12-13 01:36:20 +0000

    refactor error message to constant

--- src/virtual-pet.js
+++ src/virtual-pet.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const ERROR_MESSAGE = 'Your pet is no longer alive :(';
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
@@ -14,24 +16,24 @@ function Pet(name) {
 }
 
 Pet.prototype.growUp = function () {
-    if (!this.isAlive) throw new Error('Your pet is no longer alive :(');
+    if (!this.isAlive) throw new Error(ERROR_MESSAGE);
     this.age += 1;
     this.hunger += 5;
     this.fitness -= 3;
 }
 
 Pet.prototype.walk = function () {
-    if (!this.isAlive) throw new Error('Your pet is no longer alive :(');
+    if (!this.isAlive) throw new Error(ERROR_MESSAGE);
     this.fitness + 4 > 10 ? this.fitness = 10 : this.fitness += 4;
 }
 
 Pet.prototype.feed = function () {
-    if (!this.isAlive) throw new Error('Your pet is no longer alive :(');
+    if (!this.isAlive) throw new Error(ERROR_MESSAGE);
     this.hunger - 3 < 0 ? this.hunger = 0 : this.hunger -= 3;
 }
 
 Pet.prototype.checkUp = function () {
-    if (!this.isAlive) return 'Your pet is no longer alive :(';
+    if (!this.isAlive) return ERROR_MESSAGE;
     if (this.hunger >= 5 && this.fitness <= 3) return 'I am hungry AND I need a walk';
     else if (this.fitness <= 3) return 'I need a walk';
     else if (this.hunger >= 5) return 'I am hungry';
