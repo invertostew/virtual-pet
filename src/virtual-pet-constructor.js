6c9dcd5b7749c0222a5a4f746f652f6e75174134
Stuart Green <github.47a8z@aleeas.com>
2021-12-16 00:00:35 +0000

    converted to class syntax

---
+++ src/virtual-pet-constructor.js
@@ -0,0 +1,79 @@
+/*
+    This is my original virtual pet project
+    built with Constructor Functions, and 
+    later converted to ES6 Class Syntax.
+*/
+'use strict';
+
+const MINIMUM_AGE = 0;
+const MAXIMUM_AGE = 30;
+const MINIMUM_HUNGER = 0;
+const MAXIMUM_HUNGER = 10;
+const MINIMUM_FITNESS = 0;
+const MAXIMUM_FITNESS = 10;
+const FITNESS_THRESHOLD = 3;
+const HUNGER_THRESHOLD = 5;
+const ERROR_MESSAGE = 'Your pet is no longer alive :(';
+
+function Pet(name) {
+    this.name = name;
+    this.age = MINIMUM_AGE;
+    this.hunger = MINIMUM_HUNGER;
+    this.fitness = MAXIMUM_FITNESS;
+    this.children = [];
+
+    Object.defineProperty(this, 'isAlive', {
+        get: function () {
+            return this.age < MAXIMUM_AGE && this.hunger < MAXIMUM_HUNGER && this.fitness > MINIMUM_FITNESS;
+        }
+    });
+}
+
+Pet.prototype.growUp = function () {
+    if (!this.isAlive) throw new Error(ERROR_MESSAGE);
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+}
+
+Pet.prototype.walk = function () {
+    if (!this.isAlive) throw new Error(ERROR_MESSAGE);
+    this.fitness = Math.min(MAXIMUM_FITNESS, this.fitness + 4);
+}
+
+Pet.prototype.feed = function () {
+    if (!this.isAlive) throw new Error(ERROR_MESSAGE);
+    this.hunger = Math.max(MINIMUM_HUNGER, this.hunger - 3);
+}
+
+Pet.prototype.checkUp = function () {
+    if (!this.isAlive) return ERROR_MESSAGE;
+    else if (this.hunger >= HUNGER_THRESHOLD && this.fitness <= FITNESS_THRESHOLD) return 'I am hungry AND I need a walk';
+    else if (this.fitness <= FITNESS_THRESHOLD) return 'I need a walk';
+    else if (this.hunger >= HUNGER_THRESHOLD) return 'I am hungry';
+    return 'I feel great!';
+}
+
+Pet.prototype.haveBaby = function (name) {
+    const babyPet = new BabyPet(this, name);
+    this.children.push(babyPet);
+    return this.children.findIndex(baby => baby === babyPet);
+}
+
+function BabyPet(parent, name) {
+    Pet.call(this, name);
+
+    this.parent = parent;
+}
+
+BabyPet.prototype = Object.create(Pet.prototype);
+BabyPet.prototype.constructor = BabyPet;
+
+BabyPet.prototype.findParent = function () {
+    return `My parent is ${this.parent.name}!`;
+}
+
+module.exports = {
+    Pet,
+    BabyPet
+};
--- src/virtual-pet.js
+++ src/virtual-pet.js
@@ -10,62 +10,59 @@ const FITNESS_THRESHOLD = 3;
 const HUNGER_THRESHOLD = 5;
 const ERROR_MESSAGE = 'Your pet is no longer alive :(';
 
-function Pet(name) {
-    this.name = name;
-    this.age = MINIMUM_AGE;
-    this.hunger = MINIMUM_HUNGER;
-    this.fitness = MAXIMUM_FITNESS;
-    this.children = [];
+class Pet {
+    constructor(name) {
+        this.name = name;
+        this.age = MINIMUM_AGE;
+        this.hunger = MINIMUM_HUNGER;
+        this.fitness = MAXIMUM_FITNESS;
+        this.children = [];
+    }
 
-    Object.defineProperty(this, 'isAlive', {
-        get: function () {
-            return this.age < MAXIMUM_AGE && this.hunger < MAXIMUM_HUNGER && this.fitness > MINIMUM_FITNESS;
-        }
-    });
+    get isAlive() {
+        return this.age < MAXIMUM_AGE && this.hunger < MAXIMUM_HUNGER && this.fitness > MINIMUM_FITNESS;
+    }
+
+    growUp() {
+        if (!this.isAlive) throw new Error(ERROR_MESSAGE);
+        this.age += 1;
+        this.hunger += 5;
+        this.fitness -= 3;
+    }
+
+    walk() {
+        if (!this.isAlive) throw new Error(ERROR_MESSAGE);
+        this.fitness = Math.min(MAXIMUM_FITNESS, this.fitness + 4);
+    }
+
+    feed() {
+        if (!this.isAlive) throw new Error(ERROR_MESSAGE);
+        this.hunger = Math.max(MINIMUM_HUNGER, this.hunger - 3);
+    }
+
+    checkUp() {
+        if (!this.isAlive) return ERROR_MESSAGE;
+        else if (this.hunger >= HUNGER_THRESHOLD && this.fitness <= FITNESS_THRESHOLD) return 'I am hungry AND I need a walk';
+        else if (this.fitness <= FITNESS_THRESHOLD) return 'I need a walk';
+        else if (this.hunger >= HUNGER_THRESHOLD) return 'I am hungry';
+        return 'I feel great!';
+    }
+
+    haveBaby(name) {
+        const babyPet = new BabyPet(this, name);
+        this.children.push(babyPet);
+    }
 }
 
-Pet.prototype.growUp = function () {
-    if (!this.isAlive) throw new Error(ERROR_MESSAGE);
-    this.age += 1;
-    this.hunger += 5;
-    this.fitness -= 3;
-}
+class BabyPet extends Pet {
+    constructor(parent, name) {
+        super(name);
+        this.parent = parent;
+    }
 
-Pet.prototype.walk = function () {
-    if (!this.isAlive) throw new Error(ERROR_MESSAGE);
-    this.fitness = Math.min(MAXIMUM_FITNESS, this.fitness + 4);
-}
-
-Pet.prototype.feed = function () {
-    if (!this.isAlive) throw new Error(ERROR_MESSAGE);
-    this.hunger = Math.max(MINIMUM_HUNGER, this.hunger - 3);
-}
-
-Pet.prototype.checkUp = function () {
-    if (!this.isAlive) return ERROR_MESSAGE;
-    else if (this.hunger >= HUNGER_THRESHOLD && this.fitness <= FITNESS_THRESHOLD) return 'I am hungry AND I need a walk';
-    else if (this.fitness <= FITNESS_THRESHOLD) return 'I need a walk';
-    else if (this.hunger >= HUNGER_THRESHOLD) return 'I am hungry';
-    return 'I feel great!';
-}
-
-Pet.prototype.haveBaby = function (name) {
-    const babyPet = new BabyPet(this, name);
-    this.children.push(babyPet);
-    return this.children.findIndex(baby => baby === babyPet);
-}
-
-function BabyPet(parent, name) {
-    Pet.call(this, name);
-
-    this.parent = parent;
-}
-
-BabyPet.prototype = Object.create(Pet.prototype);
-BabyPet.prototype.constructor = BabyPet;
-
-BabyPet.prototype.findParent = function () {
-    return `My parent is ${this.parent.name}!`;
+    findParent() {
+        return `My parent is ${this.parent.name}!`;
+    }
 }
 
 module.exports = {
