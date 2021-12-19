2adaccd62c24ea0237cdd9324e162c9efeb60f69
Stuart Green <github.47a8z@aleeas.com>
2021-12-19 17:33:17 +0000

    changed global variables name

--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const globalVariables = {
+const app = {
     minimumAge: 0,
     maximumAge: 30,
     minimumHunger: 0,
@@ -15,38 +15,38 @@ const globalVariables = {
 class Pet {
     constructor(name) {
         this.name = name;
-        this.age = globalVariables.minimumAge;
-        this.hunger = globalVariables.minimumHunger;
-        this.fitness = globalVariables.maximumFitness;
+        this.age = app.minimumAge;
+        this.hunger = app.minimumHunger;
+        this.fitness = app.maximumFitness;
         this.children = [];
     }
 
     get isAlive() {
-        return this.age < globalVariables.maximumAge && this.hunger < globalVariables.maximumHunger && this.fitness > globalVariables.minimumFitness;
+        return this.age < app.maximumAge && this.hunger < app.maximumHunger && this.fitness > app.minimumFitness;
     }
 
     growUp() {
-        if (!this.isAlive) throw new Error(globalVariables.errorMessage);
+        if (!this.isAlive) throw new Error(app.errorMessage);
         this.age += 1;
         this.hunger += 5;
         this.fitness -= 3;
     }
 
     walk() {
-        if (!this.isAlive) throw new Error(globalVariables.errorMessage);
-        this.fitness = Math.min(globalVariables.maximumFitness, this.fitness + 4);
+        if (!this.isAlive) throw new Error(app.errorMessage);
+        this.fitness = Math.min(app.maximumFitness, this.fitness + 4);
     }
 
     feed() {
-        if (!this.isAlive) throw new Error(globalVariables.errorMessage);
-        this.hunger = Math.max(globalVariables.minimumHunger, this.hunger - 3);
+        if (!this.isAlive) throw new Error(app.errorMessage);
+        this.hunger = Math.max(app.minimumHunger, this.hunger - 3);
     }
 
     checkUp() {
-        if (!this.isAlive) return globalVariables.errorMessage;
-        else if (this.hunger >= globalVariables.hungerThreshold && this.fitness <= globalVariables.fitnessThreshold) return 'I am hungry AND I need a walk';
-        else if (this.fitness <= globalVariables.fitnessThreshold) return 'I need a walk';
-        else if (this.hunger >= globalVariables.hungerThreshold) return 'I am hungry';
+        if (!this.isAlive) return app.errorMessage;
+        else if (this.hunger >= app.hungerThreshold && this.fitness <= app.fitnessThreshold) return 'I am hungry AND I need a walk';
+        else if (this.fitness <= app.fitnessThreshold) return 'I need a walk';
+        else if (this.hunger >= app.hungerThreshold) return 'I am hungry';
         return 'I feel great!';
     }
 
